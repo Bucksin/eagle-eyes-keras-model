6a545d6d2bcc99c61bc0bf4c059e2668c9f2f9f6
Caroline Lin <caroline.yclin64@gmail.com>
2024-05-10 15:33:51 -0700

    Commented scripts

--- model-test.py
+++ model-test.py
@@ -1,3 +1,5 @@
+import os.path
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -14,27 +16,29 @@ print("Specified classes: ", class_names)
 img_height = 180
 img_width = 180
 
+#loading the test image into a Python Imaging Library format
 img = tf.keras.utils.load_img(
     IMAGE_FILE, target_size=(img_height, img_width)
 )
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
+img_array = tf.keras.utils.img_to_array(img) #turn image into 3D numpy(numerical python) array
+img_array = tf.expand_dims(img_array, 0) # Fill in the array if it's missing dimensions to make up a tensor
 
-new_model = tf.keras.models.load_model('exported_model/model.keras')
+new_model = tf.keras.models.load_model('exported_model/model.keras')#load the trained model
+#returns the prediction as a list of arrays in probabilities for each category in the model
+#it may be a list of raw data from the model called logits if the model is using softmax function in the last layer
 predictions = new_model.predict(img_array)
 print(predictions)
+#sigmoid function is to convert predictions to probabilities between 0 to 1 (if there's only two possible outcomes)
+# no need if last layer is not softmax
 score = float(keras.ops.sigmoid(predictions[0][0]))
 print(score)
 
-# Apply softmax function to predictions
+# Apply softmax function to predictions, it's another function to convert logits to probabilities
+# (multi-class classification problems, where there are more than two possible outcomes)
 probabilities = np.exp(predictions) / np.sum(np.exp(predictions), axis=1, keepdims=True)
 
 # Convert probabilities to percentages
 scores_percentages = probabilities * 100
 print(scores_percentages)
-for i, score in enumerate(scores_percentages[0]):
-    print(f"Score for class {class_names[i]}: {score}")
-
-
- 
-
+for i, predictions in enumerate(scores_percentages[0]):
+    print(f"Score for class {class_names[i]}: {predictions}")
--- model-train.py
+++ model-train.py
@@ -7,6 +7,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
+
 DATASET_PATH = 'model_training/dataset/flower_photo'
 data_dir = pathlib.Path(DATASET_PATH).with_suffix('')
 
@@ -17,14 +18,19 @@ batch_size = 32
 img_height = 180
 img_width = 180
 
+#Yields batches of images and labels
+#loads 80% of the images for training
+#generate td.data.Dataset from image files in a directory
 train_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
   validation_split=0.2, # set proportion of images to be used for validation
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+  subset="training", #smaller group of dataset and categorize as "training" dataset
+  seed=123,#random number for shuffling and transformation
+  image_size=(img_height, img_width),#standarized the image size
+  batch_size=batch_size)#number of samples being propogated through the next work
 
+
+#loads 20% of the images for training
 val_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
   validation_split=0.2, # set proportion of images to be used for validation
@@ -33,12 +39,12 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-class_names = train_ds.class_names
+class_names = train_ds.class_names #the categories
 print(class_names)
 
-# puts the dataset into memory to improve performance
+# puts the dataset into memory to improve performance (used for tensorflow to prefetch data)
 AUTOTUNE = tf.data.AUTOTUNE
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)#caches the elements
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 # Data normalization
@@ -51,33 +57,44 @@ first_image = image_batch[0]
 
 num_classes = len(class_names)
 
+#Defining the structure of the model (using Convolutional Neural Network)
+#Sequential means 'step-by-step' building
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),#rescales the image values from 0-255 to 0-1
+  layers.Conv2D(16, 3, padding='same', activation='relu'),#convolutional layers, basically filters to create feature maps that reps the features in the image
+  layers.MaxPooling2D(),#downsample the above so it's less to calculate
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+  layers.Flatten(),#2D feature maps into 1D vector for the next layer
+  layers.Dense(128, activation='relu'),#dense layer that uses 128 neurons and relu activation function
+  layers.Dense(num_classes)#same num of neurons as the classes
 ])
 
+#The Dense layer is a type of layer in neural networks where each neuron is connected to all neurons in the previous layer
+#this layer learns the global patterns from all pixels and not just the specified section
+#So it takes the feature id'd by the convolutional layer AND the entire image and makes a final decision
+
+
+#Config the learning process before training
+#loss function: evaluating how well the algorithm models your dataset. If the predictions off, loss function outputs a higher number
+#optimizer:tweaks the model's parameters based on the result of loss function
+#metric:used to judge the performance of your model (but not used when training the model), we're judging the modal based on accuracy
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 model.summary()
 
-epochs=10
+epochs=10 #train how many times over the whole dataset
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
 
-# Visualizing the training performance
+# Visualizing the training performance, aka. to print things out, that's it
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -100,7 +117,7 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
-# Data Augmentation
+# Data Augmentation: by flipping or rotating the images, and retrain the model again so we don't need new dataset
 data_augmentation = keras.Sequential(
   [
     layers.RandomFlip("horizontal",
@@ -114,7 +131,7 @@ data_augmentation = keras.Sequential(
 
 # Dropout
 model = Sequential([
-  data_augmentation,
+  data_augmentation,#the data augumentation layer is added
   layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
@@ -122,7 +139,7 @@ model = Sequential([
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
-  layers.Dropout(0.2),
+  layers.Dropout(0.2),#dropout layer randomly sets a fraction of the input to 0 to prevent overfitting
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes, name="outputs")
@@ -164,5 +181,7 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+print('saving model')
 # Save the model
 model.save('exported_model/model.keras')
+print('saved model')
